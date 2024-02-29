381bc9dde89c370bfc46ceb18811d197f0edca85
e <denesakin@gmail.com>
2024-02-29 21:52:04 +0100

    ui fixes
    todo:
    home page and pop ups need improvement

--- src/components/BookLeft.tsx
+++ src/components/BookLeft.tsx
@@ -1,23 +1,23 @@
 export default function BookLeft(props: 
-  {title: string,
-  author: string,
-  year_published: number,
-  cover: string,
-  weeks_number_one: number}) 
-  {
-    
+    {title: string,
+    author: string,
+    year_published: number,
+    cover: string,
+    weeks_number_one: number}) 
+    {
+
+
   return (
-    <div className='flex flex-col w-1/2 items-center relative'>
-      <img src={props.cover} alt='book cover' className='w-full sm:w-9/12  md:w-3/5 lg:w-1/2 aspect-[2/3] rounded-lg border border-white'/>
-      
-      <div className="flex flex-col bg-black p-2 gap-2 bg-opacity-80 absolute top-2 sm:top-6 xl:top-12 
-      left-1/2 transform -translate-x-1/2 text-center rounded-lg text-white w-10/12 sm:w-auto lg:w-5/12">
-        <p className="text-xs min-[375px]:text-base md:text-xl font-bold">{props.title} <span>({props.year_published})</span></p>
-        <p className="text-xs min-[375px]:text-base md:text-lg font-semibold">{props.author}</p>
-      </div>
-      
-      <p className="text-xs min-[375px]:text-base md:text-xl w-10/12 sm:w-auto font-semibold bg-black text-white 
-      p-2 absolute bottom-2 sm:bottom-6 xl:bottom-12 left-1/2 transform -translate-x-1/2 bg-opacity-80 rounded-lg">
+    <div className="w-1/2 h-full flex flex-col items-center justify-end gap-3">
+        
+        <div className="flex flex-col gap-1 text-center rounded-lg text-white">
+            <p className="text-xs min-[375px]:text-base md:text-xl lg:text-2xl font-bold leading-3">{props.title} <span>({props.year_published})</span></p>
+            <p className="text-xs min-[375px]:text-base md:text-lg lg:text-xl font-semibold">{props.author}</p>
+        </div>
+
+        <img src={props.cover} alt='book cover' className='w-full sm:w-9/12  md:w-3/5 lg:w-1/2 aspect-[2/3] rounded-lg border border-white'/>
+
+        <p className="text-xs min-[375px]:text-base md:text-lg lg:text-xl font-semibold text-white mt-px">
         Weeks #1 on the list: {props.weeks_number_one}</p>
     </div>
   )
--- src/components/BookRight.tsx
+++ src/components/BookRight.tsx
@@ -1,32 +1,31 @@
 type BookRightPropTypes = {
-  title: string,
-  author: string,
-  year_published: number,
-  cover: string,
-  handleAnswer: (answer: string) => void
-}
+    title: string,
+    author: string,
+    year_published: number,
+    cover: string,
+    handleAnswer: (answer: string) => void
+  }
+  
+  export default function BookRight({title, author, year_published, cover, handleAnswer}: BookRightPropTypes) {
+  
+    return (
+      <div className='w-1/2 h-full flex flex-col items-center justify-end gap-3'>
 
-export default function BookRight({title, author, year_published, cover, handleAnswer}: BookRightPropTypes) {
+        <div className="flex flex-col gap-1 text-center rounded-lg text-white ">
+          <p className="text-xs min-[375px]:text-base md:text-xl lg:text-2xl font-bold leading-3">{title} <span>({year_published})</span></p>
+          <p className="text-xs md:text-lg lg:text-xl font-semibold ">{author}</p>
+        </div>
 
-  return (
-    <div className='flex flex-col w-1/2 items-center relative'>
-      <img src={cover} alt='book cover' className='w-full sm:w-9/12 md:w-3/5 lg:w-1/2 aspect-[2/3] rounded-lg border border-white'/>
-
-      <div className="flex flex-col bg-black p-2 gap-2 bg-opacity-80 absolute top-2 sm:top-6 xl:top-12 
-      left-1/2 transform -translate-x-1/2 text-center rounded-lg text-white w-10/12 sm:w-auto lg:w-5/12">
-        <p className="text-xs min-[375px]:text-base md:text-xl font-bold">{title} <span>({year_published})</span></p>
-        <p className="text-xs min-[375px]:text-base md:text-lg font-semibold">{author}</p>
+        <img src={cover} alt='book cover' className='w-full sm:w-9/12  md:w-3/5 lg:w-1/2 aspect-[2/3] rounded-lg border border-white'/>
+        
+        <div className="flex gap-4 w-auto text-xs min-[375px]:text-base md:text-lg  text-white md:h-[28px] font-semibold">
+          <button className='px-2 min-[390px]:px-4 rounded-xl border border-white hover:bg-white hover:text-black' 
+          onClick={() => handleAnswer("-")}>Lower</button>
+          <button className='px-2 min-[390px]:px-3 min-[440px]:px-4 rounded-xl border border-white hover:bg-white hover:text-black' 
+          onClick={() => handleAnswer("+")}>Higher</button>
+        </div>
+        
       </div>
-      
-      <div 
-      className="flex absolute bottom-2 sm:bottom-6 xl:bottom-12 left-1/2 transform 
-      -translate-x-1/2 gap-4 w-auto text-xs min-[375px]:text-base">
-        <button className='bg-black bg-opacity-80 text-white font-bold p-2 sm:p-4 rounded-xl hover:bg-opacity-90' 
-        onClick={() => handleAnswer("-")}>Lower</button>
-        <button className='bg-black bg-opacity-80 text-white font-bold p-2 sm:p-4 rounded-xl hover:bg-opacity-90' 
-        onClick={() => handleAnswer("+")}>Higher</button>
-      </div>
-      
-    </div>
-  )
-}
+    )
+  }
+  
