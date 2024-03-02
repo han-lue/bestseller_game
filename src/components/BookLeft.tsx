08f0b971e85ec59a481bc460a0932cd26d0e15d5
e <denesakin@gmail.com>
2024-03-02 21:06:52 +0100

    font system changed to responsive- only updated the game page

--- src/components/BookLeft.tsx
+++ src/components/BookLeft.tsx
@@ -6,19 +6,21 @@ export default function BookLeft(props:
     weeks_number_one: number}) 
     {
 
+// h-[34px] min-[375px]:h-[42px] md:h-[46px]
+
 
   return (
     <div className="w-1/2 h-full flex flex-col items-center justify-end gap-3">
         
         <div className="flex flex-col gap-1 text-center rounded-lg text-white">
-            <p className="text-xs min-[375px]:text-base md:text-xl lg:text-2xl font-bold leading-3">{props.title} <span>({props.year_published})</span></p>
-            <p className="text-xs min-[375px]:text-base md:text-lg lg:text-xl font-semibold">{props.author}</p>
+            <p className="c-font-title font-extrabold">{props.title} <span>({props.year_published})</span></p>
+            <p className="c-font-author font-semibold">{props.author}</p>
         </div>
 
         <img src={props.cover} alt='book cover' className='w-full sm:w-9/12  md:w-3/5 lg:w-1/2 aspect-[2/3] rounded-lg border border-white'/>
         
-        <div className="h-[34px] min-[375px]:h-[42px] md:h-[46px] flex items-center justify-center">
-          <p className="text-xs min-[375px]:text-base md:text-lg lg:text-xl font-semibold text-white">
+        <div className="flex items-center justify-center p-2">
+          <p className="c-font-text font-bold text-white">
           Weeks #1: {props.weeks_number_one}</p>
         </div>
 
--- src/components/BookRight.tsx
+++ src/components/BookRight.tsx
@@ -12,13 +12,13 @@ type BookRightPropTypes = {
       <div className='w-1/2 h-full flex flex-col items-center justify-end gap-3'>
 
         <div className="flex flex-col gap-1 text-center rounded-lg text-white ">
-          <p className="text-xs min-[375px]:text-base md:text-xl lg:text-2xl font-bold leading-3">{title} <span>({year_published})</span></p>
-          <p className="text-xs min-[375px]:text-base md:text-lg lg:text-xl font-semibold ">{author}</p>
+          <p className="c-font-title font-extrabold">{title} <span>({year_published})</span></p>
+          <p className="c-font-author font-semibold ">{author}</p>
         </div>
 
         <img src={cover} alt='book cover' className='w-full sm:w-9/12  md:w-3/5 lg:w-1/2 aspect-[2/3] rounded-lg border border-white'/>
         
-        <div className="flex gap-4 w-auto text-xs min-[375px]:text-base md:text-lg  text-white font-semibold">
+        <div className="flex gap-4 w-full c-font-text text-white font-semibold items-center justify-center">
           <button className='p-2 min-[440px]:px-4 rounded-xl bg-white text-black active:bg-opacity-80 md:hover:opacity-80 ' 
           onClick={() => handleAnswer("-")}>Lower</button>
           <button className='p-2 min-[440px]:px-4 rounded-xl bg-white text-black active:bg-opacity-80 md:hover:opacity-80' 
