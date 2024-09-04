635503f546eca5be57104cf0e100e2284d54f552
username <denesakin@gmail.com>
2024-09-04 20:33:49 +0200

    remove unnecessary comments

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -4,9 +4,15 @@ export default function HomePage() {
   return (
     <div className='h-screen w-screen bg-gray text-white'>
       <div className='flex flex-col w-full h-full justify-center items-center gap-12 p-4 sm:p-12 md:p-36 relative'>
-          <a href="https://github.com/han-lue/bestseller_game" target="_blank" className="hover:underline c-font-title absolute top-4 font-semibold">Source code</a>
-          <p className="c-font-headline text-center">This is a simple higher or lower game where you guess which book stayed longer in The New York Times Best Seller List between 1931-2023</p>
-          <Link to="/game" relative="path"><button className='bg-white text-black p-4 c-font-text self-center rounded-lg font-semibold hover:bg-opacity-80'>Start Playing</button></Link>
+          <a href="https://github.com/han-lue/bestseller_game" target="_blank" 
+          className="hover:underline c-font-title absolute top-4 font-semibold">
+            Source code
+          </a>
+          <p className="c-font-headline text-center">
+            This is a simple higher or lower game where you guess which book stayed longer in The New York Times Best Seller List between 1931-2023
+          </p>
+          <Link to="/game" relative="path"><button className='bg-white text-black p-4 c-font-text self-center rounded-lg 
+          font-semibold hover:bg-opacity-80'>Start Playing</button></Link>
       </div>
     </div>
   )
